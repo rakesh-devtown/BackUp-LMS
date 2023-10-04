c3214b673e56dffbb245e88a75545c5500cccd25
Saumya Agrawal <saumya.agarwal@devtown.in>
2023-10-04 23:02:39 +0530

    sidebar changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import {
   LaptopOutlined,
@@ -104,4 +105,177 @@ const App = () => {
   //   </StyledLayout>
   // );
 };
+export default App;
+*/
+
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  CalendarFilled,
+  UserOutlined,
+  MessageFilled,
+  EditFilled,
+  BookFilled,
+  CompassFilled,
+  QuestionCircleFilled,
+  CloseOutlined,
+  PlusOutlined
+} from '@ant-design/icons';
+import logout from './assets/images/Logout.svg';
+import Logo from "./assets/images/devtown-vector.svg";
+
+import Cal from './components/Cal'
+import { StyledLayout } from "./styles/app.styles";
+import { FullScreenContent, StyledButton } from "./styles/shared.styles";
+import { Layout, Menu, Button, theme } from 'antd';
+import { routeDefinitions } from './constants/routes'
+import { Link } from 'react-router-dom';
+
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <StyledLayout>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          background: "#6322CC",
+        }}
+      >
+  
+        {/* <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          /> */}
+
+      </Header>
+    <Layout >
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Link to={"/"} style={{
+            background: "transparent",
+            color:'#fff',
+            padding: '10px',
+            display:'flex',
+            alignItems:'center',
+            justifyContent:'center' 
+          }}>
+            <img src={Logo} alt="logo" />
+         </Link>
+         <div style={{
+            background: "transparent",
+            display:'flex',
+            alignItems:'center',
+            justifyContent:'center',
+            fontSize: '16px',
+          }}>
+         <Button 
+          icon={collapsed ?<PlusOutlined /> : <CloseOutlined /> }
+          onClick={() => setCollapsed(!collapsed)} />
+          </div>
+        <p style={{
+            background: "transparent",
+            color:'#fff',
+            padding: '10px',
+            display:'flex',
+            alignItems:'center',
+            justifyContent:'center' 
+          }}>Menu</p>
+        <Menu
+          theme="dark"
+          mode="inline"
+          // defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: (<Link to={routeDefinitions.PROFILE}>Profile</Link>),
+            },
+            {
+              key: '2',
+              icon: <CompassFilled />,
+              label: (
+                <a href="https://www.devtown.in/bytes" target="_blank" rel="noopener noreferrer">
+                  Explore
+                </a>
+              ),
+          
+            },
+            {
+              key: '3',
+              icon: <BookFilled />,
+              label: (<Link to={routeDefinitions.PROGRAMS}>Programs</Link>),
+            },
+            {
+              key: '4',
+              icon: <CalendarFilled />,
+              label: (<Link to={routeDefinitions.SCHEDULE}>My Schedule</Link>),
+            },
+            {
+              key: '5',
+              icon: <EditFilled />,
+              label: (<Link to={routeDefinitions.PRACTICE}>Practice</Link>),
+            },          
+            {
+              key: '6',
+              icon: <MessageFilled />,
+              label: (<Link to={routeDefinitions.MESSAGE}>Message</Link>),
+            },
+          ]}
+        />
+        <p style={{
+            background: "transparent",
+            color:'#fff',
+            padding: '10px',
+            display:'flex',
+            alignItems:'center',
+            justifyContent:'center'
+          }}>Actions</p>
+        <Menu
+        theme="dark"
+        mode="inline"
+        // defaultSelectedKeys={['1']}
+        items={[
+          {
+            key: '1',
+            icon: <QuestionCircleFilled />,
+            label: (<Link to="https://www.devtown.in/contact-us">Contact Us</Link>),
+          },
+          {
+            key: '2',
+            icon: (<img src={logout} alt="logout" />),
+            label: 'Logout',
+          },
+        ]}
+        />
+      </Sider>
+      <Layout>
+        <FullScreenContent
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >          
+           <Cal/>
+          </FullScreenContent>
+      </Layout>
+    </Layout>
+    </StyledLayout>
+  );
+};
+
 export default App;
