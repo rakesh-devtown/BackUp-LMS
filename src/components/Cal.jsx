0d36eec16400806a029cc7e4f192259fce32afa1
siyapandeyvsp <siyapandeyvsp@gmail.com>
2023-10-05 14:03:45 +0530

    updated calender

--- src/components/Cal.jsx
+++ src/components/Cal.jsx
@@ -1,14 +1,61 @@
-import React from "react";
-import { StyledCaledar } from "../styles/calendar.styles";
-const App = () => {
-  const onPanelChange = (value, mode) => {
-    console.log(value.format("YYYY-MM-DD"), mode);
+import React, { useState } from 'react';
+import { Calendar, Drawer } from 'antd';
+///import {Moment} from 'moment';
+
+const CalendarScheduler = ({ events }) => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [drawerVisible, setDrawerVisible] = useState(false);
+
+  const handleDateClick = (value) => {
+    setSelectedDate(value);
+    setDrawerVisible(true);
   };
+
+  const handleCloseDrawer = () => {
+    setSelectedDate(null);
+    setDrawerVisible(false);
+  };
+
   return (
-    <StyledCaledar
-      className="my-schedule-calendar"
-      onPanelChange={onPanelChange}
-    />
+    <div>
+      <Calendar
+        onSelect={handleDateClick}
+        dateCellRender={(value) => {
+          const date = value.format('YYYY-MM-DD');
+          const eventForDate = events.find((event) => event.date === date);
+
+          if (eventForDate) {
+            return (
+              
+              <div className="event-indicator ">
+                <div className="event-indicator-dot"></div>
+              </div>
+            );
+          }
+
+          return null;
+        }}
+      />
+
+      <Drawer
+        title={`Events for ${selectedDate ? selectedDate.format('MMM D, YYYY') : ''}`}
+        width={400}
+        placement="right"
+        onClose={handleCloseDrawer}
+        visible={drawerVisible}
+      >
+        {selectedDate &&
+          events
+            .filter((event) => event.date === selectedDate.format('YYYY-MM-DD'))
+            .map((event) => (
+              <div key={event.id}>
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+              </div>
+            ))}
+      </Drawer>
+    </div>
   );
 };
-export default App;
+
+export default CalendarScheduler;
