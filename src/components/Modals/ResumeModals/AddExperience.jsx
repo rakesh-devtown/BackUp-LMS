adca2f8b45aca45745812d87ea773de4aba6323f
rakesh-devtown <rakeshmagar.fswd@devtown.in>
2024-06-21 19:15:21 +0545

    added location and employment type in add work experience

--- src/components/Modals/ResumeModals/AddExperience.jsx
+++ src/components/Modals/ResumeModals/AddExperience.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import TextArea from "antd/es/input/TextArea";
-import { Button, Checkbox, Col, Form, Input, Row, Space } from "antd";
+import { Button, Checkbox, Col, Form, Input, Row, Select, Space } from "antd";
 import useWindowSize from "../../../hooks/useWindowSize";
 import CustomDatePicker from "../../DatePicker/CustomDatePicker";
 import {
@@ -19,6 +19,7 @@ const AddExperience = ({ value }) => {
   const [checkbox2, setcheckbox2] = useState(false);
   const { width } = useWindowSize();
 
+  const { Option } = Select;
   const handleCheckbox1 = () => setcheckbox1(!checkbox1);
   const handleCheckbox2 = () => setcheckbox2(!checkbox2);
   const handleSubmit = (e) => {
@@ -107,6 +108,28 @@ const AddExperience = ({ value }) => {
             Present
           </Checkbox>
         </Form.Item>
+        <Row gutter={15} style={{ marginBottom: "25px" }}>
+          <Col span={12}>
+            <Form.Item name="locationType" label="Location type">
+              <Select placeholder="Please select" allowClear>
+                <Option value="onSite">On-site</Option>
+                <Option value="hybrid">Hybrid</Option>
+                <Option value="remote">Remote</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item name="employmentType" label="Employment type">
+              <Select placeholder="Please select" allowClear>
+                <Option value="fullTime">Full-time</Option>
+                <Option value="partTime">Part-time</Option>
+                <Option value="internship">Internship</Option>
+                <Option value="freelance">Freelance</Option>
+                <Option value="selfEmployed">Self-employed</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
         <Form.Item label="Description" name={"description"}>
           <TextArea rows={5} />
         </Form.Item>
