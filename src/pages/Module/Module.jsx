178cfa62e3ec40f1b7d528c21d0bb8e3a9ae0b6b
rakesh-devtown <rakeshmagar.fswd@devtown.in>
2024-06-23 16:46:25 +0545

    module page updated

--- src/pages/Module/Module.jsx
+++ src/pages/Module/Module.jsx
@@ -14,31 +14,22 @@ import useWindowSize from "../../hooks/useWindowSize";
 import RightSiderMenu from "../../components/RightSiderMenu/RightSiderMenu";
 import FolderDetailsCard from "../../components/Cards/video/FolderDetailsCard";
 import CousreProgress from "../../components/Cards/module/CourseProgress";
-import { treeData } from "./mockData";
 import useBatchStore from "../../store/batchStore";
-import ModuleCardHeader from "../../components/ModuleTree2/ModuleCardHeader";
-import TopicCard from "../../components/ModuleTree2/TopicCard";
+import ModuleChapter from "../../components/Module/ModuleChapter";
 
 const Module = () => {
-  const [selectedKey, setSelectedKey] = useState([]);
   const { width } = useWindowSize();
-  const navigate = useNavigate();
-  const currentCourseDetails = useBatchStore((state) => state.currentCourseDetails);
+  const currentCourseDetails = useBatchStore(
+    (state) => state.currentCourseDetails
+  );
   const { Content, Sider } = Layout;
-  const [mockData, setMockData] = useState([]);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
 
-  //function for tree nodes
-  const onSelect = (selectedKeys, info) => {
-    // navigate("/video");
-    console.log("selected", selectedKeys, info);
-    setSelectedKey(selectedKeys);
-  };
-  const onCheck = (checkedKeys, info) => {
-    console.log("onCheck", checkedKeys, info);
-  };
+  const chapterNameArray = [
+    "FrontEnd Development",
+    "BackendDevelopment",
+    "DSA",
+    "others",
+  ];
 
   const mySiderStyle = {
     background: "transparent",
@@ -54,39 +45,11 @@ const Module = () => {
   // const rightSidebarWidth = width >= 992 ? "60px" : "0";
   const rightSidebarWidth = 0;
 
-
-
-  // useEffect(() => {
-  //   if(currentCourseDetails?.sections && currentCourseDetails?.sections.length > 0){
-  //     const data = currentCourseDetails.sections.map((section, index) => {
-  //       return {
-  //         title: (
-  //           <ModuleCardHeader
-  //             title={section?.name}
-  //             topic1={section?.subsections[0].name}
-  //             topic2={section?.subsections[1].name}
-  //             onGoing={section?.subsections.length > 0 ? true : false}
-  //           />
-  //         ),
-  //         key: index.toString(),
-  //         children: section?.subsections.map((subSection, subIndex) => {
-  //           return {
-  //             title: <TopicCard data={subSection} />,
-  //             key: `${index}-${subIndex}`,
-  //             className: `${subIndex === section?.subsections?.length -1 ? "animate-bounce" : ""}`,
-  //           };
-          
-  //         })
-  //       };
-  //     });
-  //     setMockData(data);
-  //   }
-  // },[currentCourseDetails])
-
   return (
     <Layout>
       <Helmet>
-        <title>DevTown - {currentCourseDetails?.name}</title>
+        <title>Mock title</title>
+        {/* <title>DevTown - {currentCourseDetails?.name}</title> */}
         <meta name="settings" content="settings" />
         <link rel="canonical" href="https://www.learn.devtown.in/setting" />
       </Helmet>
@@ -122,18 +85,13 @@ const Module = () => {
               </Button>
             </Link>
           </ModuleTop>
+
+          {/* showing all the chapter and modules */}
           <ModuleBody>
             <h4>Explore Modules for Learning</h4>
-            <Tree
-              multiple
-              expandedKeys={selectedKey}
-              blockNode
-              onSelect={onSelect}
-              onCheck={onCheck}
-              treeData={treeData}
-              switcherIcon={false}
-              onExpand={(e) => console.log(e)}
-            />
+            {chapterNameArray.map((ele, ind) => (
+              <ModuleChapter chapterName={ele} index={ind} />
+            ))}
           </ModuleBody>
         </MainContainer>
       </Content>
@@ -200,29 +158,6 @@ const ModuleBody = styled.div`
     border-radius: 8px;
     background: #e6ebf3;
     margin-bottom: 5px;
-  }
-  .ant-tree-switcher {
-    width: 0;
-  }
-  .ant-tree-indent {
-    width: 0;
-  }
-  .ant-tree-treenode {
-    padding: 0;
-  }
-  .ant-tree-node-selected {
-    .ant-tree-title > div {
-      border-bottom: none;
-    }
-    .closed {
-      display: none;
-    }
-    .open {
-      display: grid;
-    }
-    .box-bottom {
-      display: none;
-    }
   }
 `;
 
